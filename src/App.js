bc413e12ead187461601225aa0b70757cc06f3d2
mkang7446 <mkang7446@gmail.com>
2022-03-02 14:49:20 -0800

    hook useEffect, useState and fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 
 function App() {
-  const timeSeries = "TIME_SERIES_DAILY";
-  const apiInfo = {
+  const [data, setData] = useState([]);
+  const [timeSeries, setTimeSeries] = useState("TIME_SERIES_DAILY");
+  const [symbol, setSymbol] = useState("APPL");
+
+  const searchOption = {
     key: process.env.REACT_APP_ALPHAV_KEY,
     api: "https://www.alphavantage.co/",
   };
-  console.log(apiInfo.key);
+
+  function getData() {
+    const url = `${searchOption.api}query?function=${timeSeries}&symbol=${symbol}&apikey=${searchOption.key}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch(console.error);
+  }
+
+  useEffect(() => {
+    getData(data);
+  }, []);
 
   return (
     <div className="App">

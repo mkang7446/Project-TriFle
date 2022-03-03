4fae948b96f26f48f84e69d351adab9f34c729c4
mkang7446 <mkang7446@gmail.com>
2022-03-02 16:10:46 -0800

    add links to about, news pages in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes, Link } from "react-router-dom";
+import SearchForm from "./SearchForm";
 
 function Header(props) {
   return (
@@ -9,6 +10,13 @@ function Header(props) {
           <h1>Trifle</h1>
           <h2>For your Financial Freedom</h2>
         </div>
+      </Link>
+      <SearchForm />
+      <Link to="/about">
+        <h2>About</h2>
+      </Link>
+      <Link to="/news">
+        <h2>News</h2>
       </Link>
     </div>
   );
